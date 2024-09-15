3014e2208cc2b826efe1ee84e03e1a04e8700e49
AlexeyTolmachov <taar12sh@gmail.com>
2024-09-15 10:50:30 +0300

    add mailer

---
+++ src/mailer/mailer.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { MailerService } from './mailer.service';
+import { MailerDto } from './dto/mailer_send.dto';
+
+@Controller('email')
+export class MailerController {
+    constructor(private readonly mailerService: MailerService) { }
+
+    @Post('send')
+    async sendEmail(@Body() dto: MailerDto) {
+        const { to, subject, text, html } = dto
+        return this.mailerService.sendMail(to, subject, text, html);
+    }
+}
