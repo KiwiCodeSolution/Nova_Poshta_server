40db5a3a8deda2fdddadd65ae86c166b3bfee35e
AlexeyTolmachov <taar12sh@gmail.com>
2024-09-16 17:05:18 +0300

    update schema

--- src/news/schemas/news.schema.ts
+++ src/news/schemas/news.schema.ts
@@ -3,35 +3,35 @@ import { Document } from 'mongoose';
 
 @Schema()
 export class News extends Document {
-    @Prop({ required: true })
-    title: string;
+  @Prop({ required: true })
+  title: string;
 
-    @Prop({ required: true, unique: true })
-    slug: string;
+  @Prop({ required: true, unique: true })
+  slug: string;
 
-    @Prop({ type: [String], required: true })
-    sections: string[];
+  @Prop({ type: [String], required: true })
+  sections: string[];
 
-    @Prop({ default: 0 })
-    views: number;
+  @Prop({ default: 0 })
+  views: number;
 
-    @Prop({ default: Date.now })
-    createdAt: Date;
+  @Prop({ default: Date.now })
+  createdAt: Date;
 
-    @Prop({ required: true })
-    author: string;
+  @Prop({ required: true })
+  author: string;
 
-    @Prop({ type: [String] })
-    metaTags: string[];
+  @Prop({ type: [String] })
+  metaTags: string[];
 
-    @Prop({ required: true })
-    content: string;
+  @Prop({ required: true })
+  content: string;
 
-    @Prop({ enum: ['published', 'created', 'archived'], default: 'created' })
-    status: string;
+  @Prop({ enum: ['published', 'created', 'archived'], default: 'created' })
+  status: string;
 
-    @Prop({ type: Date })
-    publishDate: Date;
+  @Prop({ type: Date })
+  publishDate: Date;
 }
 
 export const NewsSchema = SchemaFactory.createForClass(News);
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,24 +1,24 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
+  @Prop({ required: true })
+  username: string;
 
-    @Prop({ required: true })
-    username: string;
+  @Prop({ required: true, unique: true })
+  email: string;
 
-    @Prop({ required: true, unique: true })
-    email: string;
+  @Prop({ required: true })
+  phone: string;
 
-    @Prop({ required: true })
-    phone: string;
+  @Prop({ required: true })
+  password: string;
 
-    @Prop({ required: true })
-    password: string;
-    @Prop()
-    userId: string;
+  @Prop({ type: Types.ObjectId })
+  _id: Types.ObjectId;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
