4f70feca581f900e5d5c746be07bba71fedbd09d
AlexeyTolmachov <taar12sh@gmail.com>
2024-09-16 17:02:56 +0300

    add setings  validation pipe

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   await app.listen(5000);
 }
 bootstrap();
