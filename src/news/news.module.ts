70cb2df2259e9d5a635a81ef70911f8993d6827c
AlexeyTolmachov <taar12sh@gmail.com>
2024-09-16 17:03:50 +0300

    add collection mongodb

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NewsController } from './news.controller';
 import { NewsService } from './news.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { News, NewsSchema } from './schemas/news.schema';
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: News.name, schema: NewsSchema }]), // Додаємо модель тут
+  ],
   controllers: [NewsController],
-  providers: [NewsService]
+  providers: [NewsService],
 })
 export class NewsModule {}
