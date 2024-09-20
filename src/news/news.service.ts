e5dc9d08321683076ea9140d8f2fff31858a4170
AlexeyTolmachov <taar12sh@gmail.com>
2024-09-20 07:42:09 +0300

    update function slag

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,21 +1,26 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { News } from './schemas/news.schema';
 import { CreateNewsDto } from './dto/createNews.dto';
 import { UpdateNewsDto } from './dto/updateNews.dto';
-import { User, UserDocument } from 'src/users/schemas/user.schema';
 import { LoggerService } from 'src/utils/logging/logger.service';
+import { slugify } from 'transliteration';
 
 @Injectable()
 export class NewsService {
-    constructor(@InjectModel(News.name) 
+    constructor(@InjectModel(News.name)
     private newsModel: Model<News>,
-    private readonly loggerService: LoggerService
-) { }
+        private readonly loggerService: LoggerService
+    ) { }
+
 
     async create(createNewsDto: CreateNewsDto): Promise<News> {
         const slug = this.generateSlug(createNewsDto.title);
+        const existingNews = await this.newsModel.findOne({ slug });
+        if (existingNews) {
+            throw new ConflictException(`News with slug "${slug}" already exists`);
+        }
         const news = new this.newsModel({ ...createNewsDto, slug });
         return news.save();
     }
@@ -41,26 +46,26 @@ export class NewsService {
         if (!updatedNews) throw new NotFoundException('News not found');
         return updatedNews;
     }
-// зробити перевірку на наявність юзера за таким айді та при видаленні зробити логування ім'я та емейла того хто видаляв 
+    // зробити перевірку на наявність юзера за таким айді та при видаленні зробити логування ім'я та емейла того хто видаляв 
+    // або отримувати з фронта  данные
     async delete(slug: string, userId: string): Promise<void> {
         const news = await this.newsModel.findOneAndDelete({ slug }).exec();
         if (news) {
-          // Логирование удаления через LoggerService
-          await this.loggerService.logDeletion(news.title, userId);
-          console.log('Новость успешно удалена и залогирована.');
+            await this.loggerService.logDeletion(news.title, userId);
+            console.log('Новость успешно удалена и залогирована.');
         } else {
-          throw new NotFoundException('News not found');
+            throw new NotFoundException('News not found');
         }
-      }
-    
+    }
+
     private generateSlug(title: string): string {
-        return title
-            .toLowerCase()
+        return slugify(title, { lowercase: true })
             .replace(/[^a-z0-9]+/g, '-')
             .replace(/^-+|-+$/g, '');
     }
 
-  
+
+
 
 
 }
