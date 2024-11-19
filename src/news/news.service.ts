9416a5ed040a5076ef11fec83e0a2ded1b53d0b6
AlexeyTolmachov <taar12sh@gmail.com>
2024-11-19 14:53:39 +0200

    fixed img before text

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -57,21 +57,38 @@ export class NewsService {
 
         return news;
     }
+    // private extractTextFromParagraphs(content: string): string {
+    //     const paragraphRegex = /<p[^>]*>(.*?)<\/p>/g;
+    //     let match: RegExpExecArray | null;
+    //     let paragraphText = '';
+    //     while ((match = paragraphRegex.exec(content)) !== null) {
+    //         paragraphText += match[1];
+    //     }
+    //     paragraphText = paragraphText.replace(/&[^;]+;/g, '');
+    //     paragraphText = paragraphText.replace(/[\u{1F600}-\u{1F6FF}]/gu, '');
+    //     let preview = paragraphText.trim().substring(0, 100);
+    //     preview += '...';
+
+    //     return preview;
+    // }
     private extractTextFromParagraphs(content: string): string {
         const paragraphRegex = /<p[^>]*>(.*?)<\/p>/g;
         let match: RegExpExecArray | null;
         let paragraphText = '';
+    
         while ((match = paragraphRegex.exec(content)) !== null) {
             paragraphText += match[1];
         }
+        
+        paragraphText = paragraphText.replace(/<\/?[^>]+(>|$)/g, '');
         paragraphText = paragraphText.replace(/&[^;]+;/g, '');
         paragraphText = paragraphText.replace(/[\u{1F600}-\u{1F6FF}]/gu, '');
         let preview = paragraphText.trim().substring(0, 100);
         preview += '...';
-
+    
         return preview;
     }
-
+    
 
     async create(createNewsDto: CreateNewsDto): Promise<News> {
         const slug = this.generateSlug(createNewsDto.title);
